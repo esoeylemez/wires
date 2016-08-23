4f87241d5d5732fc3bf04dede81e5fe53b4a2c8c
Ertugrul Söylemez <esz@posteo.de>
2016-08-23 23:29:15 +0200

    Add main Control.Wire module.

---
+++ Control/Wire.hs
@@ -0,0 +1,17 @@
+-- |
+-- Copyright:  (c) 2016 Ertugrul Söylemez
+-- License:    BSD3
+-- Maintainer: Ertugrul Söylemez <esz@posteo.de>
+-- Stability:  experimental
+
+module Control.Wire
+    ( -- * Wires
+      module Control.Category,
+      module Control.Wire.Core,
+      module Control.Wire.Varying
+    )
+    where
+
+import Control.Category
+import Control.Wire.Core
+import Control.Wire.Varying
