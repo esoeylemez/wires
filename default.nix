929e6298d0f35ebcd6e84c78597c0b96c0634640
Ertugrul Söylemez <esz@posteo.de>
2016-08-23 21:52:08 +0200

    Update default.nix.

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-{ mkDerivation, base, profunctors, semigroupoids, stdenv, these }:
+{ mkDerivation, base, deepseq, profunctors, semigroupoids, stdenv
+, these
+}:
 mkDerivation {
   pname = "wires";
   version = "0.1.0";
   src = ./.;
-  libraryHaskellDepends = [ base profunctors semigroupoids these ];
+  libraryHaskellDepends = [
+    base deepseq profunctors semigroupoids these
+  ];
   homepage = "https://github.com/esoeylemez/wires";
   description = "Functional reactive programming library";
   license = stdenv.lib.licenses.bsd3;
